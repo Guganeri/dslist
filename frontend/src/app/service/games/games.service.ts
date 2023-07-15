09e5a27b310e8a2e231b4762a6c982b57836ae2b
bvalmeida <bvalmeida94@gmail.com>
2023-07-15 12:13:00 -0300

    impl - env for frontend, docker front and docker-compose

--- frontend/src/app/service/games/games.service.ts
+++ frontend/src/app/service/games/games.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IGameModel } from 'src/app/models/game-model';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,10 @@ export class GamesService {
 
   constructor(private httpClient: HttpClient) { }
 
-  resourceFoundAllGames = 'http://localhost:8080/games';
+  apiUrl = environment.apiGamesUrl;
+  resourceFoundAllGames = 'games';
 
   findAllGames(): Observable<IGameModel[]>{
-    return this.httpClient.get<IGameModel[]>(`${this.resourceFoundAllGames}`)
+    return this.httpClient.get<IGameModel[]>(`${this.apiUrl}${this.resourceFoundAllGames}`)
   }
 }
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiGamesUrl: 'http://localhost:8080/'
+};

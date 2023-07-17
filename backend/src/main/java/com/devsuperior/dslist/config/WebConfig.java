0e4779aa4b51ae7cc1121af280ef82abe4937828
Guga Neri <guganeriss@gmail.com>
2023-07-17 01:35:22 +0100

    cors config

---
+++ backend/src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.config;
+
+public class WebConfig {
+
+    @Configuration
+    public class WebConfig {
+
+        @Value("${cors.origins}")
+        private String corsOrigins;
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+                }
+            };
+        }
+
+    }
+
+}
